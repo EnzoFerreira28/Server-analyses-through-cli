d868d25b1369b8f153127b75431e5d8daf73380f
Enzo Silva Ferreira <enzosfcontato@gmail.com>
2026-04-08 21:37:53 -0300

    Adiciona o arquivo ScriptTeste a pasta de ShellScript

---
+++ ShellScript/scriptTeste.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+echo "Estou testando se consegui acessar corretamente uma subpasta do meu repositório no github
+e então utilizando o Neovim para criar um arquvio shell para commitar ele."
